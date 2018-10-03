524a7161f450fb2896d8449dfd2fc02267f1b997
DESKTOP-FPREBHK\germe <unity.amconsoft1@gmail.com>
2018-10-03 17:45:34 +0300

    NestedScrollRect veritcal swipe

--- Assets/UpperMenuBehaviourController.cs
+++ Assets/UpperMenuBehaviourController.cs
@@ -2,67 +2,103 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class UpperMenuBehaviourController : MonoBehaviour {
 
+    public bool IsDraggingNow;
+    [Header("Percent of visibiblity when SearchPanel will hide")]
+    [Range(1,90)]
+    public float PercentToHideSearchPanel;
+    [Header("Show , hide panel animation speed")]
+    [SerializeField]
+    private float _animationSpeed;
     [SerializeField]
     private RectTransform _searchPanel;
     [SerializeField]
     private RectTransform _appPanel;
+    [SerializeField]
+    private RectTransform _appPanelContent;
 
-    private Vector2 _searchPanelStartPosition;
-    private Vector2 _searchPanelStartSize;
-    private Vector2 _appPanelStartPosition;
-    private Vector2 _appPanelStartSize;
+    private ScrollRect _appPanelScrollRect;
+    private bool _canHideSearchPanel = false;
+    private bool _isSearchPanelHidenNow;
+    private float _searchPanelSize;
 
-    private Vector2 _searchPanelPanelHidePosition;
-    private Vector2 _appPanelFullscreenPosition;
-    private Vector2 _appPanelFullscreenSize;
+    /// <summary>
+    /// Move SearchPanel Y position
+    /// </summary>
+    /// <param name="newYPos">Acceleration by Y</param>
+    public void MoveSearchPanelByY(float newYPos)
+    {
+        if (_searchPanel.anchoredPosition.y <= _searchPanelSize && _searchPanel.anchoredPosition.y >= 0 && _canHideSearchPanel)
+        {
+            _searchPanel.anchoredPosition = new Vector2(_searchPanel.anchoredPosition.x, _searchPanel.anchoredPosition.y - newYPos);
+        }
+    }
 
-    private bool _canHide;
+    /// <summary>
+    /// Check SearchPanel by show percent variable , then show or hide 
+    /// </summary>
+    public void SnapToBorder()
+    {
+        if (_canHideSearchPanel)
+        {
+            if (_searchPanel.anchoredPosition.y < _searchPanelSize / (100/PercentToHideSearchPanel))
+                ShowSearchPanel(true);
+            else
+                ShowSearchPanel(false);
+        }
+    }
 
     private void Awake()
     {
-        _searchPanelStartPosition = _searchPanel.localPosition;
-        _searchPanelStartSize = _searchPanel.sizeDelta;
-
-        _appPanelStartPosition = _appPanel.anchoredPosition;
-        _appPanelStartSize = _appPanel.sizeDelta;
-
-       // _searchPanelPanelHidePosition = new Vector2(_searchPanelStartPosition.x,_searchPanelStartPosition.y-_searchPanelStartSize.y);
-
+        _searchPanelSize = _searchPanel.sizeDelta.y;
+        _appPanelScrollRect = _appPanel.GetComponent<ScrollRect>();
     }
     private void ShowSearchPanel(bool isShow)
     {
-        _searchPanel.anchoredPosition = isShow ? _searchPanelStartSize : Vector2.zero;
+        _searchPanel.DOAnchorPos(isShow ? Vector2.zero : new Vector2(0, _searchPanelSize), _animationSpeed);
+        _isSearchPanelHidenNow = !isShow;
+    }
 
-    }
-    private void ShowAppPanel(bool isShow)
-    {
-        _appPanel.sizeDelta = new Vector2(_appPanel.sizeDelta.x,isShow ? 0 : _appPanelStartSize.y);
-        //_appPanel.anchoredPosition = new Vector2(_appPanel.anchoredPosition.x, isShow ? 0 : _appPanelStartPosition.y);
-       // _appPanel.DOSizeDelta()
-        _appPanel.DOAnchorPos(new Vector2(_appPanel.anchoredPosition.x, isShow ? 0 : _appPanelStartPosition.y),1);
-    }
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.A))
-        {
-            ShowSearchPanel(true);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            ShowSearchPanel(false);
-        }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            ShowAppPanel(true);
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            ShowAppPanel(false);
-        }
+        CheckHideSearchPanelPossibility();
+        LimitSearchPanelPosition();
+    }
 
+    private void CheckHideSearchPanelPossibility()
+    {
+        if (_appPanelContent.anchoredPosition.y >= _searchPanelSize)
+        {
+            _canHideSearchPanel = true;
+        }
+        if (_appPanelContent.anchoredPosition.y <= _searchPanelSize)
+        {
+            _canHideSearchPanel = false;
+        }
+    }
+    private void LimitSearchPanelPosition()
+    {   
+        if (!IsDraggingNow)
+        {
+            if(!_canHideSearchPanel && _isSearchPanelHidenNow)
+            {
+                ShowSearchPanel(true);
+            }
+        }
+        else
+        {
+            if (_searchPanel.anchoredPosition.y > _searchPanelSize)
+            {
+                _searchPanel.anchoredPosition = new Vector2(0, _searchPanelSize);
+            }
+            else if (_searchPanel.anchoredPosition.y < 0)
+            {
+                _searchPanel.anchoredPosition = Vector2.zero;
+            }
+        }       
     }
 
 }
---
+++ Assets/UpperMenuDragHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UpperMenuDragHandler : MonoBehaviour, IBeginDragHandler ,IDragHandler,IEndDragHandler
+{
+    public UpperMenuBehaviourController UpperMenuBehaviourController;
+
+    private ScrollRect _scrollRect;
+    private Vector2 _startTouchPosition;
+    private float _lastTouchYPosition;
+
+    public void Awake()
+    {
+        _scrollRect = GetComponent<ScrollRect>();       
+    }
+
+    public void OnBeginDrag(PointerEventData data)
+    {
+        _startTouchPosition = Input.mousePosition;
+        _lastTouchYPosition = _startTouchPosition.y;
+        UpperMenuBehaviourController.IsDraggingNow = true;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        float currentTouchYPositionDifference = _startTouchPosition.y - Input.mousePosition.y;
+        float acceleration = currentTouchYPositionDifference - _lastTouchYPosition;
+        _lastTouchYPosition = currentTouchYPositionDifference;        
+        UpperMenuBehaviourController.MoveSearchPanelByY(acceleration);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        UpperMenuBehaviourController.IsDraggingNow = false;
+        UpperMenuBehaviourController.SnapToBorder();
+    }
+}
